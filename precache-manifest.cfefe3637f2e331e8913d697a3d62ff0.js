fbb114c59e859cd717d3fd4c7c08aa7ad903482e
elenawq <60143171+elenawq@users.noreply.github.com>
2020-06-09 16:53:05 +0300

    Updates

---
+++ precache-manifest.cfefe3637f2e331e8913d697a3d62ff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2676f9b47e2b10d6b7d7e4f34558ac2c",
+    "url": "/recipe-maker/index.html"
+  },
+  {
+    "revision": "cb2b5822a112d4cce6f6",
+    "url": "/recipe-maker/static/css/main.68d707c7.chunk.css"
+  },
+  {
+    "revision": "560867f2f0ac1326db2d",
+    "url": "/recipe-maker/static/js/2.85b302e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-maker/static/js/2.85b302e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb2b5822a112d4cce6f6",
+    "url": "/recipe-maker/static/js/main.5a726f0a.chunk.js"
+  },
+  {
+    "revision": "e3959a0c27fa0caa2d32",
+    "url": "/recipe-maker/static/js/runtime-main.81b62f96.js"
+  }
+]);
